15b76d605f1213f308a83b70040f0d522b328178
Gaizka01 <ggaretxana@gmail.com>
2014-05-08 22:27:20 +0100

    intentona

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 #To be executed with raw data txt file in working directory
-data = read.table("household_power_consumption.txt", skip = 66637, nrows = 2880, header = F, sep = ";", col.names = colnames(read.table("household_power_consumption.txt", nrows = 1, sep = ";", header = TRUE)))
 png(filename = "plot2.png", width = 480, height = 480)
+data = read.table("household_power_consumption.txt", skip = 66637, nrows = 2880, header = F, sep = ";", col.names = colnames(read.table("household_power_consumption.txt", nrows = 1, sep = ";", header = TRUE)))
 wDays <- strptime(paste(data$Date, data$Time), format='%d/%m/%Y %H:%M:%S')
 plot(wDays, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,10 +1,11 @@
 #To be executed with raw data txt file in working directory
-data = read.table("household_power_consumption.txt", skip = 66637, nrows = 2880, header = F, sep = ";", col.names = colnames(read.table("household_power_consumption.txt", nrows = 1, sep = ";", header = TRUE)))
 png(filename = "plot4.png", width = 480, height = 480)
+data = read.table("household_power_consumption.txt", skip = 66637, nrows = 2880, header = F, sep = ";", col.names = colnames(read.table("household_power_consumption.txt", nrows = 1, sep = ";", header = TRUE)))
+wDays <- strptime(paste(data$Date, data$Time), format='%d/%m/%Y %H:%M:%S')
 par(mfrow = c(2,2))
 with(data, {
-plot(WDays, data$Global_active_power, type ="l", ylab = "Global Active Power", xlab ="")
-plot(WDays, data$Voltage, type ="l", ylab = "Voltage", xlab = "datetime")
-plot(WDays, data$Sub_metering_1, type ="l", ylab = "Energy sub metering", xlab = ""); lines (WDays, data$Sub_metering_2, col="red"); lines(WDays, data$Sub_metering_3, col = "blue")
-plot(WDays, data$Global_reactive_power, type ="l", ylab = "Global_reactive_power", xlab = "datetime")})
+plot(wDays, data$Global_active_power, type ="l", ylab = "Global Active Power", xlab ="")
+plot(wDays, data$Voltage, type ="l", ylab = "Voltage", xlab = "datetime")
+plot(wDays, data$Sub_metering_1, type ="l", ylab = "Energy sub metering", xlab = ""); lines (wDays, data$Sub_metering_2, col="red"); lines(wDays, data$Sub_metering_3, col = "blue")
+plot(wDays, data$Global_reactive_power, type ="l", ylab = "Global_reactive_power", xlab = "datetime")})
 dev.off()
