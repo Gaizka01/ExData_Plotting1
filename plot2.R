ebed0b4bd4df643fad080b7751bf635e554fc870
Gaizka01 <ggaretxana@gmail.com>
2014-05-08 21:59:39 +0100

    intento3

--- plot2.R
+++ plot2.R
@@ -1,4 +1,5 @@
 #To be executed with raw data txt file in working directory
+data = read.table("household_power_consumption.txt", skip = 66637, nrows = 2880, header = F, sep = ";", col.names = colnames(read.table("household_power_consumption.txt", nrows = 1, sep = ";", header = TRUE)))
 png(filename = "plot2.png", width = 480, height = 480)
 wDays <- strptime(paste(data$Date, data$Time), format='%d/%m/%Y %H:%M:%S')
 plot(wDays, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
--- plot3.R
+++ plot3.R
@@ -1,4 +1,5 @@
 #To be executed with raw data txt file in working directory
+data = read.table("household_power_consumption.txt", skip = 66637, nrows = 2880, header = F, sep = ";", col.names = colnames(read.table("household_power_consumption.txt", nrows = 1, sep = ";", header = TRUE)))
 png(filename = "plot3.png", width = 480, height = 480)
 plot(WDays, data$Sub_metering_1, type ="l", ylab = "Energy sub metering", xlab = ""); lines (WDays, data$Sub_metering_2, col="red"); lines(WDays, data$Sub_metering_3, col = "blue")
 dev.off()
